95e483a09da9c9ceb970072a0d2d03c3b523e5ce
Jairus Tan <jt3x2022@gmail.com>
2025-08-14 20:51:38 +0800

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const tempDiv = document.getElementById('tempValue');
-
-// Simulate live temperature updates
-setInterval(() => {
-    const temp = (30 + Math.random() * 10).toFixed(2); // random 30-40°C
-    tempDiv.textContent = `${temp} °C`;
-}, 1000); // update every second
